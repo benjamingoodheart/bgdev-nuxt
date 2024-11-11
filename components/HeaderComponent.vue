c234cedf1d4f5f3001154956dcd0e77aee7b743a
benjamingoodheart <bgoodheart@fordham.edu>
2024-11-11 12:04:52 -0500

    more

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,26 +1,27 @@
 import { UAvatar, USkeleton, UHorizontalNavigation, UDropdown } from '../.nuxt/components';
 <script setup lang="ts">
-import {useBreakpoints, breakpointsTailwind} from '@vueuse/core'
-const breakpoints = useBreakpoints(breakpointsTailwind)
-const largerThanSm = breakpoints.greater('sm')
-const links = [{label: 'home'}, {label: 'resume'}]
+import {useMediaQuery} from '@vueuse/core'
+const isLargeScreen = useMediaQuery('(min-width: 1024px)')
+const links = [{label: 'portfolio'}, {label: 'resume'}, {label:'currently'}, {label:'contact'}]
 const dropdownItems = [[
-    {label: 'home'},{label:'resume'}
+{label: 'portfolio'}, {label: 'resume'}, {label:'currently'}, {label:'contact'}
 ]]
+
+
 </script>
 <template>
     <UContainer>
-    <div class="grid grid-cols-6 md:grid-cols-4 border-2 border-primary-500 dark:border-0 dark:bg-gray-500 rounded-xl my-5 py-2 px-2">
+    <div class="grid grid-cols-6 md:grid-cols-6 border-2 border-primary-500 dark:border-0 dark:bg-gray-500 rounded-xl my-5 py-2 px-2">
         <div class="text-green-200">    
             <USkeleton class="md:h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
-            <USkeleton class="h-8 w-8" v-if="!largerThanSm"/>
+            <USkeleton class="h-8 w-8" v-if="!isLargeScreen"/>
         </div>
         <div class="my-auto col-span-4">
-           <h1 class="text-xl">benjamin.goodheart.dev</h1>
+           <UButton  variant="link" class="dark:text-gray-100"><h1 class="text-xl">benjamin.goodheart.dev</h1></UButton>
         </div>
         <div class="justify-self-end">
-            <UHorizontalNavigation :links="links" v-if="largerThanSm"></UHorizontalNavigation>
-            <UDropdown v-if="!largerThanSm" :items="dropdownItems">
+            <UHorizontalNavigation :links="links" v-if="isLargeScreen"></UHorizontalNavigation>
+            <UDropdown v-if="!isLargeScreen" :items="dropdownItems">
                 <UButton icon="ic:outline-menu" ></UButton>
             </UDropdown>
         </div>
---
+++ components/PortfolioComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+const items = [{
+    slot: 'data',
+    label: 'Project #1',
+}, {
+    slot:'data',
+    label: 'Project #2',
+}]
+</script>
+<template>
+    <UTabs :items="items">
+        <template #data="{item}">
+            <UCard>
+                <template #header>
+                    <h1 class="text-xl">{{ item.label }}</h1>
+                </template>
+                <div class="min-h-20 bg-primary-400 text-center">
+                    <h1 class="text-white">IMAGE</h1>
+                </div>
+                <UDivider icon="hugeicons:arrow-down-double" class="my-4"/>
+                <div>
+
+                </div>
+            </UCard>
+        </template>
+    </UTabs>
+</template>
