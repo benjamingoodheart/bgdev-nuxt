4d4353fed694cbafe0253f475977d4ab81d17495
benjamingoodheart <bgoodheart@fordham.edu>
2024-11-11 12:51:32 -0500

    Update PortfolioComponent.vue

--- components/PortfolioComponent.vue
+++ components/PortfolioComponent.vue
@@ -2,6 +2,8 @@
 const items = [{
     slot: 'data',
     label: 'Project #1',
+    desc: 'lorem ipsum whatever',
+    stack: ['Vue.js', 'node.js'],
 }, {
     slot:'data',
     label: 'Project #2',
@@ -17,9 +19,17 @@ const items = [{
                 <div class="min-h-20 bg-primary-400 text-center">
                     <h1 class="text-white">IMAGE</h1>
                 </div>
-                <UDivider icon="hugeicons:arrow-down-double" class="my-4"/>
+                <UDivider class="my-4"/>
                 <div>
+                    <h1 class="text-xl my-2">Description</h1>
+                    <p>{{ item.desc }}</p>
 
+                    <h1 class="text-xl my-2">
+                        Stack: </h1>
+                        <ul>
+                            <li v-for="s in item.stack"><Icon name="ant-design:caret-right-filled" />{{ s }}</li>
+                        </ul>
+                    <h1 class="text-xl my-2">Demo</h1>
                 </div>
             </UCard>
         </template>
